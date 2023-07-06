19b700468c7d05dac3810ecfafcf47076712a259
최서윤 <cinnamon1995@naver.com>
2023-07-07 01:56:41 +0900

    eslintrc까지 설정 완료

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  //render함수 DOM에 컴포넌트를 랜더링하는 함수
+  //인자로 랜더링할 React 컴포넌트가 들어감
+  render(<App />)
+  const linkElement = screen.getByText(/learn react/i)
+  expect(linkElement).toBeInTheDocument()
+
+  const lintTest = screen.getAllByRole('button', {
+    name: 'lintTest',
+  })
+  expect(lintTest).toHaveTextContent('lineTest')
+})
